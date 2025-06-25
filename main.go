80733e3a252c989525284540f918bb0b02b1acdb
Dirza1 <jasper.olthof@xs4all.nl>
2025-06-25 22:46:14 +0200

    ensured server is working

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package chirpy
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	httpServeMux := http.ServeMux{}
+	httpServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	srv := &http.Server{
+		Addr:    ":8090",
+		Handler: &httpServeMux,
+	}
+	log.Fatal(srv.ListenAndServe())
+
+}
