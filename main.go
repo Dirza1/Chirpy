09d1dc4150df5ff2eebb5053e6a62bceceab363f
jasper.olthof <jasper.olthof@thermofisher.com>
2025-06-26 15:15:19 +0200

    Added healthz handler

--- main.go
+++ main.go
@@ -6,12 +6,22 @@ import (
 )
 
 func main() {
-	httpServeMux := http.ServeMux{}
-	httpServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	mux := http.ServeMux{}
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	srv := &http.Server{
 		Addr:    ":8090",
-		Handler: &httpServeMux,
+		Handler: &mux,
 	}
+
+	mux.HandleFunc("/healthz", healthz)
+
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func healthz(writer http.ResponseWriter, request *http.Request) {
+	text := []byte("OK")
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write(text)
 }
